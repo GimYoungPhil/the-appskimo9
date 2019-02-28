d5c211773d34fdedaa9da8f89c96217d8011f676
GimYoungPhil <youngphil.gim@gmail.com>
2019-02-28 13:19:27 +0900

    fixed bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppsComponent } from './apps/apps.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/apps', pathMatch: 'full' },
   { path: 'apps', component: AppsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import {
-  MatToolbarModule,
-  MatButtonModule,
-  MatCheckboxModule,
-} from '@angular/material';
-import {MatIconModule} from '@angular/material/icon';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { AppsComponent } from './apps/apps.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     AppsComponent,
+    AboutComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
